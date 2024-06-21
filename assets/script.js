a8235f8aea40c830b45ffc8c0360ae1fa9807873
emiliemalo <145115342+emiliemalo@users.noreply.github.com>
2024-06-21 13:12:29 +0200

    Modification du JS

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,4 @@
 
-
-
-
-
-/*CODE DE OPENCLASSROOM*/
-
-const img_slides = [
-  {
-    image: "slide1.jpg",
-    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
-  },
-  {
-    image: "slide2.jpg",
-    tagLine:
-      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-  },
-  {
-    image: "slide3.jpg",
-    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
-  },
-  {
-    image: "slide4.png",
-    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
-  },
-];
-
 /*
 
 étape 1 : générer une slide
@@ -38,13 +12,15 @@ générer le html qui correspond à la slide
 
 // étape 1 => pour une seule image
 
-let i = 0;
+/*let i = 0;
 
 const image = document.getElementById("slides");
 image.src = "./assets/images/slideshow/" + img_slides[i].image
 
 const tag = document.getElementById("tagline");
-tag.innerHTML = img_slides[i].tagLine;
+tag.innerHTML = img_slides[i].tagLine; 
+*/
+
 
  const img_slides = [
 	{
@@ -65,39 +41,32 @@ tag.innerHTML = img_slides[i].tagLine;
 	}
 ]
 
-let i = 0;
 
+const banner = document.querySelector ("banner");
 const image = document.getElementById("slides");
 const tag = document.getElementById("tagline");
 const arrowLeft = document.getElementById("left")
 const arrowRight = document.getElementById("right")
 
+let i=0;
 
+function activeSlide (){
 
-function updateslides () {
-	image.src = "./assets/images/slideshow/" + img_slides[i].image;
-	tag.innerHTML = img_slides[i].tagLine;
-}
-
-function nextSlide (){
-
-	i++;
-    if (i >= img_slides.length) {
-        i = 0;
-    }
-    updateSlide();
-}
-
-function previousSlide() {
-    i--;
-    if (i < 0) {
-        i = img_slides.length- 1;
-    }
-    updateSlide();
-
-	updateSlide();
-
+	banner.src = slides [i].image;
+	taglineElement.innerHTML = slides[i].tagLine;
 
 }
-arrowLeft.addEventListener('click', previousSlide);
-arrowRight.addEventListener('click', nextSlide);
+
+
+
+arrowRight.addEventListener("click", () => {
+	i = (i + 1) % slides.length;
+	activeSlide();
+
+arrowLeft.addEventListener("click", () => {
+		i = (i - 1 + slides.length) % slides.length;
+		activeSlide();
+
+
+
+
