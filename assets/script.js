611cb851bff8e1dc6f3781a75b3c7fabea00ea46
emiliemalo <145115342+emiliemalo@users.noreply.github.com>
2024-06-21 12:42:19 +0200

    Reproduction exercice ajout event listener

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,11 @@
 
 let banner = document.getElementById(slides)
 
-arrowLeft.addEventListener('click')
-arrowRight.addEventListener('click')
 
 
 /*CODE DE OPENCLASSROOM*/
 
-img.src= const slides = [
+ const img_slides = [
 	{
 		"image":"slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
@@ -26,4 +24,39 @@ img.src= const slides = [
 	}
 ]
 
+let i = 0;
 
+const image = document.getElementById("slides");
+const tag = document.getElementById("tagline");
+const arrowLeft = document.getElementById("left")
+const arrowRight = document.getElementById("right")
+
+
+
+function updateslides () {
+	image.src = "./assets/images/slideshow/" + img_slides[i].image;
+	tag.innerHTML = img_slides[i].tagLine;
+}
+
+function nextSlide (){
+
+	i++;
+    if (i >= img_slides.length) {
+        i = 0;
+    }
+    updateSlide();
+}
+
+function previousSlide() {
+    i--;
+    if (i < 0) {
+        i = img_slides.length- 1;
+    }
+    updateSlide();
+
+	updateSlide();
+
+
+}
+arrowLeft.addEventListener('click', previousSlide);
+arrowRight.addEventListener('click', nextSlide);
